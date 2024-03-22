74c60d33ede440635f8dfa4afa0332d9c6df1790
Mia Battad <mbattad@me.com>
2024-03-22 12:39:54 -0500

    remove Command enum for simplicity

--- src/actions/ControlSota.java
+++ src/actions/ControlSota.java
@@ -8,42 +8,31 @@ import jp.vstone.sotatalk.TextToSpeechSota;
 
 public class ControlSota {
 
-    // Commands the operator can run from the scanner
-    enum Command {
-        HI("hi"),
-        BYE("bye");
-    
-        public String label;
-        private Command(String label) {
-            this.label = label;
-        }
-    }
-
     public static void main(String [] args) {
-        CRobotMem mem = new CRobotMem();
+        //CRobotMem mem = new CRobotMem();
         Scanner scanner = new Scanner(System.in);
 
-        if(!mem.Connect()) {
-            System.out.println("Error connecting to robot's memory");
-        }
+        //if(!mem.Connect()) {
+        //    System.out.println("Error connecting to robot's memory");
+        //}
 
         System.out.println("User's name: ");
         String username = scanner.nextLine();
 
         while(scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            Command command = Command.valueOf(input);
+            String command = input.split(" ")[0];
 
             switch(command) {
-                case HI:
+                case "hi":
                     // Sota says "hi" to the user
                     System.out.println("Greeting user "+username);
-                    CPlayWave.PlayWave(TextToSpeechSota.getTTS("ハイ"+username));
+                    //CPlayWave.PlayWave(TextToSpeechSota.getTTS("ハイ"+username));
                     break;
-                case BYE:
+                case "bye":
                     // Sota says "bye" to the user
                     System.out.println("Exiting");
-                    CPlayWave.PlayWave(TextToSpeechSota.getTTS("バイ"));
+                    //CPlayWave.PlayWave(TextToSpeechSota.getTTS("バイ"));
                     break;
                 default:
                     break;
